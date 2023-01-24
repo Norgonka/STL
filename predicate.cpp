a5b130368c70db55f21f35deba483517fc31bea2
Norgonka <82911658+Norgonka@users.noreply.github.com>
2023-01-24 02:32:24 +0200

    Add files via upload

---
+++ predicate.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Person
+{
+public:
+
+	Person(string name, double score)
+	{
+		this->Name = name;
+		this->Score = score;
+	}
+	
+	bool operator () (const Person& p)
+	{
+		return p.Score > 180;
+	}
+
+	string Name;
+	int Score;
+};
+
+void SortPeopleByName(vector<Person> people)
+{
+
+	sort(people.begin(), people.end(), [](Person& p1, Person& p2)
+	{
+			return p1.Name < p2.Name;
+	});
+
+	for (auto element : people)
+	{
+		cout << "Name:\t" << element.Name << "\tScore:\t" << element.Score << endl;
+	}
+}
+
+vector<Person> ReturnPassedStudents(vector<Person> people)
+{
+	vector<Person> passedStudents;
+	copy_if(people.begin(), people.end(), back_inserter(passedStudents), people.front());
+	return passedStudents;
+}
+
+void ShowPassedStudents(vector<Person> people)
+{
+
+	auto result = find_if(people.begin(), people.end(), people.front());
+
+	if (result == people.end())
+	{
+		cout << "Noone passed" << endl;
+	}
+	else
+	{
+		cout << "List of students that passed:" << endl;
+
+		for (auto it : ReturnPassedStudents(people))
+		{
+			cout << it.Name << endl;
+		}
+	}
+}
+
+int main()
+{
+	vector<Person> people
+	{	Person("Peter", 200),
+		Person("Marry", 181),
+		Person("John", 145),
+		Person("Glenn", 181),
+		Person("Martin", 122),
+		Person("Linda", 169),
+		Person("Victor", 193),
+		Person("Stan", 184),
+		Person("Bruce", 199),
+		Person("Sydney", 179)
+	};
+
+	SortPeopleByName(people);
+	cout << "===========================================================" << endl;
+
+	cout << "Count of students that passed:\t" << count_if(people.begin(), people.end(), people.front()) << endl;
+
+	ShowPassedStudents(people);
+}
---
+++ std_function.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+void FillVector(vector<int> &vc)
+{
+    int a;
+
+    cout << "Input integer. Input 0 or anything else to quit out of cycle." << endl;
+    while (true)
+    {
+        cin >> a;
+        if (!a) break;
+
+        vc.emplace_back(a);
+    }
+}
+
+void Foo(int a)
+{
+    if (10 <= a && a <= 40)
+    {
+        cout << a << " is in range of 10 to 40." << endl;
+    }
+}
+
+void Bar(int a)
+{
+    if (a % 2 == 0)
+    {
+        cout << a << " is even" << endl;
+    }
+}
+
+void DoWork(vector<int>& vc, vector<function<void(int)>> fVector)
+{
+    for (auto it : vc)
+    {
+        for (auto fit : fVector)
+        {
+            fit(it);
+        }
+    }
+}
+
+int main()
+{
+    vector<int> vc;
+    
+    FillVector(vc);
+
+    vector<function<void(int)>> fVector;
+
+    fVector.emplace_back(Foo);
+    fVector.emplace_back(Bar);
+
+    DoWork(vc, fVector);
+}
